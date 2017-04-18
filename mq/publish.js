2cd4ec87da187e8a66067f3f59605668dc3ffdb0
anguangying <anguangying@sina.cn>
2017-04-18 14:03:19 +0800

    MQ connection publish client amend

--- mq/publish.js
+++ mq/publish.js
@@ -5,22 +5,24 @@ var Stomp = require('stomp-client');
 var config = require('../properties/config');
 var client = new Stomp(config.get('activeMq.host'), config.get('activeMq.port'),config.get('activeMq.userId'), config.get('activeMq.pwd'));
 var p = {};
-    client.connect(function(sessionId) {
-        console.log('connect OK,sessionId='+sessionId);
-    });
-
-p.publish = function (destination,content) {
-    client.on('connect',function (connected) {
-        if (connected){
+/**
+ * [{destination,content},{destination,content},...]
+ * @param destination
+ * @param content
+ */
+p.publish = function (destContents) {
+    client.connect(function(){
+        for (let destContent of destContents){
+            let destination =  destContent.destination;
+            let content =  destContent.content;
             client.publish(destination,content);
-        }else {
-            client.connect(function(sessionId) {
-                console.log('connect OK,sessionId='+sessionId);
-                client.publish(destination,content);
-            });
         }
     });
-
+};
+p.publish = function (destination,content) {
+    client.connect(function(){
+        client.publish(destination,content);
+    });
 };
 
 module.exports = p;
