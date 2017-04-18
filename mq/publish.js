00af9f4f18910ed68e4ec2e2fd6df795aa73f9da
anguangying <anguangying@sina.cn>
2017-04-18 13:28:52 +0800

    MQ stomp connection init;

--- mq/publish.js
+++ mq/publish.js
@@ -5,12 +5,17 @@ var Stomp = require('stomp-client');
 var config = require('../properties/config');
 var client = new Stomp(config.get('activeMq.host'), config.get('activeMq.port'),config.get('activeMq.userId'), config.get('activeMq.pwd'));
 var p = {};
+    client.connect(function(sessionId) {
+        console.log('connect OK,sessionId='+sessionId);
+    });
+
 p.publish = function (destination,content) {
     client.on('connect',function (connected) {
         if (connected){
             client.publish(destination,content);
         }else {
             client.connect(function(sessionId) {
+                console.log('connect OK,sessionId='+sessionId);
                 client.publish(destination,content);
             });
         }
